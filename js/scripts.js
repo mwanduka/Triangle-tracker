4b2f4045a96a775ccc5b5a98b061848b3d94f34d
mwanduka <42966698+mwanduka@users.noreply.github.com>
2018-09-16 23:18:37 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
  var isoceles = function(side1,side2,side3){
-    return side1===side2 || side1===side3 || side2===side3;
-  }
+   return side1===side2 || side1===side3 || side2===side3;
+ }
 
-  var equilateral = function(side1,side2,side3){
-    return side1===side2 && side1===side3;
-  }
+ var equilateral = function(side1,side2,side3){
+   return side1===side2 && side1===side3;
+ }
 
-  var scalene = function(side1,side2,side3){
-    return side1 != side2 && side1 != side3 && side2 != side3;
-  }
+ var scalene = function(side1,side2,side3){
+   return side1 != side2 && side1 != side3 && side2 != side3;
+ }
 
 var side1 = parseInt(prompt('side1');
 var side2 = parseInt(prompt('side2');
@@ -17,14 +17,17 @@ var triangle = [side1, side2, side3];
 
 if((side1 >= side2+side3)||(side2 >= side1+side3)||(side3 >= side2+side1)){
       ("#typeoftriangle").text("not triangle");
-      alert(nottriangle);
+      alert(not a triangle);
     } else if(equilateral(side1,side2,side3)===true){
       ("#typeoftriangle").text("an equilateral");
       alert(equilateral);
-    } else if(isoceles(side1, side2, side3)===true){
+    } else if(isoceles(side1,side2,side3)===true){
       ("#typeoftriangle").text("an isoceles");
       alert(isoceles);
     } else {
       ("#typeoftriangle").text("a scalene");
       alert(scalene);
     }
+    prompt("#finalized").text(triangleType);
+    ('#output').show();
+      });
