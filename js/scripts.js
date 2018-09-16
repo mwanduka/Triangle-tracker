3e536a91a9a2316377b3a659459ea8538e1eb95c
mwanduka <42966698+mwanduka@users.noreply.github.com>
2018-09-16 14:17:16 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+ var isoceles = function(side1,side2,side3){
+    return side1===side2 || side1===side3 || side2===side3;
+  }
+
+  var equilateral = function(side1,side2,side3){
+    return side1===side2 && side1===side3;
+  }
+
+  var scalene = function(side1,side2,side3){
+    return side1 != side2 && side1 != side3 && side2 != side3;
+  }
+
 var side1 = parseInt(prompt('side1');
 var side2 = parseInt(prompt('side2');
 var side3 = parseInt(prompt('side3');
-var triangle = [side1, side2];
+var triangle = [side1, side2, side3];
 
 if((side1 >= side2+side3)||(side2 >= side1+side3)||(side3 >= side2+side1)){
       ("#typeoftriangle").text("not triangle");
