5f8dca1fe999e695aa3741e6df9c322f2853036b
mwanduka <42966698+mwanduka@users.noreply.github.com>
2018-09-17 10:01:59 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
- function triangle(){
-
-var side1 = parseFloat(document.getElementById('side1').value);
-var side2 = parseFloat(document.getElementById('side2').value);
-var side3 = parseFloat(document.getElementById('side3').value);
+ var side3 = parseFloat(document.getElementById('side3').value);
 
 var side1side2 = side1 + side2;
 var side1side3 = side1 + side3;
@@ -23,3 +19,6 @@ if((side1 >= side2+side3)||(side2 >= side1+side3)||(side3 >= side2+side1)){
     alert(word);
       });
     }
+function reload() {
+  location.reload();
+}
